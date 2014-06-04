47bbd994939a9b0fcd8ff8d0a91883d62ad2e5bd
neopilatos <neopilatos@gmail.com>
2014-06-04 11:20:37 -0500

    mejoramiento bash
    mejoramiento bash

--- recipes/joomla.rb
+++ recipes/joomla.rb
@@ -2,10 +2,11 @@ bash "install_Joomla" do
   user "root"
   cwd "/tmp"
   code <<-EOH
+  echo "Rute"
+  mkdir /var/www/html/joomla
+  cd /var/www/html/joomla
   wget http://joomlacode.org/gf/download/frsrelease/17410/76021/Joomla_2.5.7-Stable-Full_Package.tar.gz
   tar zxvf Joomla_2.5.7-Stable-Full_Package.tar.gz  -C /var/www/html
   cd /var/www/html/Joomla_2.5.7-Stable-Full_Package
-  ./configure
-  make install
   EOH
 end
