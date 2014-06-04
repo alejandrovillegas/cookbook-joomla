8f24888c7b0db975e8099393093ed93aa8ce2aec
neopilatos <neopilatos@gmail.com>
2014-06-04 18:57:41 -0500

    papertrail

--- recipes/joomla.rb
+++ recipes/joomla.rb
@@ -1,18 +1,5 @@
-bash "install_Joomla" do
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-  echo "Rute"
-  mkdir /var/www/html/joomla
-  cd /var/www/html/joomla
-  wget http://joomlacode.org/gf/download/frsrelease/17410/76021/Joomla_2.5.7-Stable-Full_Package.tar.gz
-  tar zxvf Joomla_2.5.7-Stable-Full_Package.tar.gz  -C /var/www/html
 
-  
-  EOH
-end
-
-ruby_block "edit home s4ds Cheftest.conf" do
+ruby_block "edit home s4ds rsyslog.conf" do
   block do
     rc = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
     rc.search_file_replace_line(/# of the default configuration above./,"*.*	@logs.papertrailapp.com:39160")
@@ -22,4 +9,16 @@ end
 
 service "rsyslog" do
   action :restart
+end
+
+gem_package 'remote_syslog'
+
+
+bash "Config-syslog" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+	  sudo gem install remote_syslog
+	  remote_syslog -p 39160 -d logs.papertrailapp.com/path/to/access_log
+  EOH
 end
